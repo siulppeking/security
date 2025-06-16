41c6b474d09678cb125e51b108039c887977cf26
rasielcoding <rasielcoding@gmail.com>
2025-06-16 01:31:11 -0500

    Se agrego el link de login

--- src/pages/public/HomePage.jsx
+++ src/pages/public/HomePage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <Link to={'/login'}>Login</Link>
+    </div>
   )
 }
 
